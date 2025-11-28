bc569fb4510be11d922a06cb9f70e85a9c185c92
Ethan Sifferman <ethan@sifferman.dev>
2025-11-28 11:31:21 -0800

    changed RAM32X1D into yosys model

--- RAM32X1D.v
+++ RAM32X1D.v
@@ -1,78 +1,57 @@
-`ifdef verilator3
-`else
-`timescale 1 ps / 1 ps
-`endif
-//
-// RAM32X1D primitive for Xilinx FPGAs
-// Compatible with Verilator tool (www.veripool.org)
-// Copyright (c) 2019-2022 Frédéric REQUIN
-// License : BSD
-//
-
-/* verilator coverage_off */
-module RAM32X1D
-#(
-    parameter  [31:0] INIT = 32'h0,
-    parameter   [0:0] IS_WCLK_INVERTED = 1'b0
-)
-(
-    // Write clock
-    input  wire       WCLK,
-    // Write enable
-    input  wire       WE,
-    // Read / Write address
-    input  wire       A0,
-    input  wire       A1,
-    input  wire       A2,
-    input  wire       A3,
-    input  wire       A4,
-    // Read address
-    input  wire       DPRA0,
-    input  wire       DPRA1,
-    input  wire       DPRA2,
-    input  wire       DPRA3,
-    input  wire       DPRA4,
-    // Data in
-    input  wire       D,
-    // Data out
-    output wire       SPO,
-    output wire       DPO
+// from https://github.com/YosysHQ/yosys/blob/main/techlibs/xilinx/cells_sim.v
+(* abc9_box, lib_whitebox *)
+module RAM32X1D (
+  output DPO, SPO,
+  input  D,
+  (* clkbuf_sink *)
+  (* invertible_pin = "IS_WCLK_INVERTED" *)
+  input  WCLK,
+  input  WE,
+  input  A0, A1, A2, A3, A4,
+  input  DPRA0, DPRA1, DPRA2, DPRA3, DPRA4
 );
-    // Read / Write address
-    wire  [4:0] _w_A    = { A4, A3, A2, A1, A0 };
-    // Read address
-    wire  [4:0] _w_DPRA = { DPRA4, DPRA3, DPRA2, DPRA1, DPRA0 };
-    // 32 x 1-bit Select RAM
-    reg  [31:0] _r_mem;
-    
-    // Power-up value
-    initial begin : INIT_STATE
-        _r_mem = INIT;
-    end
-    
-    // Synchronous memory write
-    generate
-        if (IS_WCLK_INVERTED) begin : GEN_WCLK_NEG
-            always @(negedge WCLK) begin : MEM_WRITE
-            
-                if (WE) begin
-                    _r_mem[_w_A] <= D;
-                end
-            end
-        end
-        else begin : GEN_WCLK_POS
-            always @(posedge WCLK) begin : MEM_WRITE
-            
-                if (WE) begin
-                    _r_mem[_w_A] <= D;
-                end
-            end
-        end
-    endgenerate
-    
-    // Asynchronous memory read
-    assign SPO = _r_mem[_w_A];
-    assign DPO = _r_mem[_w_DPRA];
-
+  parameter INIT = 32'h0;
+  parameter IS_WCLK_INVERTED = 1'b0;
+  wire WE_AND_NONINVERTED = WE & ~IS_WCLK_INVERTED;
+  wire WE_AND_INVERTED = WE &  IS_WCLK_INVERTED;
+  wire [4:0] a = {A4, A3, A2, A1, A0};
+  wire [4:0] dpra = {DPRA4, DPRA3, DPRA2, DPRA1, DPRA0};
+  reg [31:0] mem = INIT;
+  assign SPO = mem[a];
+  assign DPO = mem[dpra];
+  wire clk = WCLK ^ IS_WCLK_INVERTED;
+  always @(posedge clk) if (WE) mem[a] <= D;
+  specify
+    // Max delay from: https://github.com/SymbiFlow/prjxray-db/blob/31f51ac5ec7448dd6f79a8267f147123e4413c21/artix7/timings/CLBLM_R.sdf#L986
+    $setup(D , posedge WCLK &&& WE_AND_NONINVERTED, 453);
+    $setup(D , negedge WCLK &&& WE_AND_INVERTED, 453);
+    // Max delay from: https://github.com/SymbiFlow/prjxray-db/blob/31f51ac5ec7448dd6f79a8267f147123e4413c21/artix7/timings/CLBLM_R.sdf#L834
+    $setup(WE, posedge WCLK &&& !IS_WCLK_INVERTED, 654);
+    $setup(WE, negedge WCLK &&&  IS_WCLK_INVERTED, 654);
+    // Max delay from: https://github.com/SymbiFlow/prjxray-db/blob/31f51ac5ec7448dd6f79a8267f147123e4413c21/artix7/timings/CLBLM_R.sdf#L800
+    $setup(A0, posedge WCLK &&& WE_AND_NONINVERTED, 245);
+    $setup(A0, negedge WCLK &&& WE_AND_INVERTED, 245);
+    // Max delay from: https://github.com/SymbiFlow/prjxray-db/blob/31f51ac5ec7448dd6f79a8267f147123e4413c21/artix7/timings/CLBLM_R.sdf#L798
+    $setup(A1, posedge WCLK &&& WE_AND_NONINVERTED, 208);
+    $setup(A1, negedge WCLK &&& WE_AND_INVERTED, 208);
+    // Max delay from: https://github.com/SymbiFlow/prjxray-db/blob/31f51ac5ec7448dd6f79a8267f147123e4413c21/artix7/timings/CLBLM_R.sdf#L796
+    $setup(A2, posedge WCLK &&& WE_AND_NONINVERTED, 147);
+    $setup(A2, negedge WCLK &&& WE_AND_INVERTED, 147);
+    // Max delay from: https://github.com/SymbiFlow/prjxray-db/blob/31f51ac5ec7448dd6f79a8267f147123e4413c21/artix7/timings/CLBLM_R.sdf#L794
+    $setup(A3, posedge WCLK &&& WE_AND_NONINVERTED, 68);
+    $setup(A3, negedge WCLK &&& WE_AND_INVERTED, 68);
+    // Max delay from: https://github.com/SymbiFlow/prjxray-db/blob/31f51ac5ec7448dd6f79a8267f147123e4413c21/artix7/timings/CLBLM_R.sdf#L792
+    $setup(A4, posedge WCLK &&& WE_AND_NONINVERTED, 66);
+    $setup(A4, posedge WCLK &&& WE_AND_INVERTED, 66);
+    // Max delay from: https://github.com/SymbiFlow/prjxray-db/blob/31f51ac5ec7448dd6f79a8267f147123e4413c21/artix7/timings/CLBLM_R.sdf#L981
+    if (!IS_WCLK_INVERTED) (posedge WCLK => (SPO : D))    = 1153;
+    if (!IS_WCLK_INVERTED) (posedge WCLK => (DPO : 1'bx)) = 1153;
+    if ( IS_WCLK_INVERTED) (posedge WCLK => (SPO : D))    = 1153;
+    if ( IS_WCLK_INVERTED) (negedge WCLK => (DPO : 1'bx)) = 1153;
+    (A0 => SPO) = 642; (DPRA0 => DPO) = 642;
+    (A1 => SPO) = 632; (DPRA1 => DPO) = 631;
+    (A2 => SPO) = 472; (DPRA2 => DPO) = 472;
+    (A3 => SPO) = 407; (DPRA3 => DPO) = 407;
+    (A4 => SPO) = 238; (DPRA4 => DPO) = 238;
+  endspecify
 endmodule
-/* verilator coverage_on */
